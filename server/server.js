7eea854b5118aedf4f5183a5531845fd21d94933
StefanIngimar <stefan.ingimarsson@outlook.com>
2024-04-15 17:48:22 +0200

    added server

---
+++ server/server.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express()
+
+app.get("/calendar", (req, res) => {
+    res.json()
+})
+
+app.listen(3000, () => {console.log("Server started on port 3000")})
