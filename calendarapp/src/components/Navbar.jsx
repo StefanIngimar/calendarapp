3f3efa8b1486f9b69bb4e2bda8b268209e240831
StefanIngimar <stefan.ingimarsson@outlook.com>
2024-04-16 23:04:51 +0200

    logout and found some sport apis

--- calendarapp/src/components/Navbar.jsx
+++ calendarapp/src/components/Navbar.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import { useClerk } from "@clerk/clerk-react";
+import { useNavigate } from 'react-router-dom';
 
 const TopNavBar = ({children}) => {
+  const { signOut } = useClerk();
+  const navigate = useNavigate();
+  const handleSignOut = async () => {
+    await signOut();
+    navigate("/starter");
+  };
+
   return (
-    
 
 <nav class="bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700 padding-bottom: 20px">
   <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
@@ -49,7 +57,7 @@ const TopNavBar = ({children}) => {
           <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Pricing</a>
         </li>
         <li>
-          <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Account</a>
+          <button onClick={handleSignOut} href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Sign Out</button>
         </li>
       </ul>
     </div>
